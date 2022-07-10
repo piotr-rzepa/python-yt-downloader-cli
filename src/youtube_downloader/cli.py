862f16efc4514ece9d8abff58105d64a35794e36
Piotr Rzepkowski <56642437+piotr-rzepa@users.noreply.github.com>
2022-07-10 03:11:13 +0200

    Project setup: Formatting, linting, static type checking (#3)
    Linting: Add linting, type checking, formatting
    
    * Add formatting code using Black
    * Add coding style enforcement using Flake8
    * Add static type checking using mypy
    * Add multiple plugins for Flake8 to tighten the strictness
    * Create pre-commit configuration for pre-commit hooks
    * Use newly installed tools to refactor, lint, format code
    * Add missing type annotations inside python scripts
    * Add section about formatting, linting and type checking

--- src/youtube_downloader/cli.py
+++ src/youtube_downloader/cli.py
@@ -1,53 +1,113 @@
 """Youtube videos downloader
 
+
 This script allows the user to download youtube video from given URL.
+
 The possible downloaded files, contain both audio and video as a single file (progressive)
 
 """
 
 import os
+import sys
+
 import click
 import pytube
-import sys
 import tqdm
+
 from . import __version__
 
+
 @click.command()
-@click.option('--url', help="A YouTube watch URL", required=True, type=str)
-@click.option('-r', '--resolution', help="Resolution of the video to download. If not specified, uses highest resolution possible.", required=False, type=str)
-@click.option('-o', '--output-path', help="Output path for downloaded media file. Defaults to current working directory", required=False, type=str)
-@click.option('-f', '--filename', help="Output filename for downloaded media file. Defaults to the video's filename.", required=False, type=str)
+@click.option("--url", help="A YouTube watch URL", required=True, type=str)
+@click.option(
+    "-r",
+    "--resolution",
+    help="Resolution of the video to download. If not specified, uses highest resolution possible.",
+    required=False,
+    type=str,
+)
+@click.option(
+    "-o",
+    "--output-path",
+    help="Output path for downloaded media file. Defaults to current working directory",
+    required=False,
+    type=str,
+)
+@click.option(
+    "-f",
+    "--filename",
+    help="Output filename for downloaded media file. Defaults to the video's filename.",
+    required=False,
+    type=str,
+)
 @click.version_option(version=__version__)
 def main(url: str, resolution: str, output_path: str, filename: str) -> None:
+
     try:
+
         yt = pytube.YouTube(url)
-        progresive_streams = yt.streams.filter(progressive=True)
+
+        progressive_streams = yt.streams.filter(progressive=True)
+
         if not resolution:
-            click.secho(f"⚠️ No resolution specified. Downloading video in highest resolution possible: {progresive_streams.get_highest_resolution().resolution}", fg="yellow")
-            stream = progresive_streams.get_highest_resolution()
+
+            click.secho(
+                f"⚠️ No resolution specified. Downloading video in highest resolution possible: {progressive_streams.get_highest_resolution().resolution}",
+                fg="yellow",
+            )
+
+            stream = progressive_streams.get_highest_resolution()
+
         else:
-            stream = progresive_streams.get_by_resolution(resolution)
+
+            stream = progressive_streams.get_by_resolution(resolution)
+
             if not stream:
-                click.secho(f"⚠️ {resolution} resolution not available for the video. Possible video resolutions: {[stream.resolution for stream in progresive_streams.order_by('resolution')]}", fg="yellow")
+
+                click.secho(
+                    f"⚠️ {resolution} resolution not available for the video. Possible video resolutions: {[stream.resolution for stream in progressive_streams.order_by('resolution')]}",
+                    fg="yellow",
+                )
+
                 sys.exit(1)
-        
-        pbar = tqdm.tqdm(total=stream.filesize, unit="bytes", unit_scale=True, unit_divisor=1000, desc=filename or stream.title)
-        
-        def on_progress(stream: pytube.Stream, chunk: bytes, bytes_remaining: int) -> None:
+
+        pbar = tqdm.tqdm(
+            total=stream.filesize,
+            unit="bytes",
+            unit_scale=True,
+            unit_divisor=1000,
+            desc=filename or stream.title,
+        )
+
+        def on_progress(
+            stream: pytube.Stream, chunk: bytes, bytes_remaining: int
+        ) -> None:
+
             pbar.update(len(chunk))
-            
+
         def on_complete(stream: pytube.Stream, file_path: str) -> None:
+
             pbar.close()
-            click.secho(f"✔️ File {filename or stream.title} saved at {os.path.normpath(file_path)} in {stream.resolution} resolution.", fg="green")
-            
+
+            click.secho(
+                f"✔️ File {filename or stream.title} saved at {os.path.normpath(file_path)} in {stream.resolution} resolution.",
+                fg="green",
+            )
+
         yt.register_on_progress_callback(on_progress)
+
         yt.register_on_complete_callback(on_complete)
-        
+
         stream.download(output_path=output_path, filename=filename)
-        
+
     except pytube.exceptions.VideoUnavailable:
+
         click.secho(f"❌ Video {url} is unavailable.", fg="red")
+
         sys.exit(1)
+
     except pytube.exceptions.RegexMatchError:
+
         click.secho(f"❌ URL watch link {url} is invalid.", fg="red")
+
         sys.exit(1)
--- tests/stubs.py
+++ tests/stubs.py
@@ -1,12 +1,13 @@
-from typing import List
+from typing import Any, List
+
 
 class MockStream:
     def __init__(self, res: str, file: str, title: str) -> None:
         self.resolution = res
         self.filesize = file
         self.title = title
-        
-    def download(self, *args, **kwargs) -> None:
+
+    def download(self, *args: str, **kwargs: Any) -> None:
         pass
 
 
@@ -16,27 +17,28 @@ class MockStreamsArray:
         self.streams = [
             MockStream("144p", "file-144p", "file-title-144p"),
             MockStream("360p", "file-360p", "file-title-360p"),
-            MockStream("720p", "file-720p", "file-title-720p")
-            ]
-    def filter(self, *args, **kwargs):
+            MockStream("720p", "file-720p", "file-title-720p"),
+        ]
+
+    def filter(self, *args: str, **kwargs: Any):
         return self
-    
+
     def get_highest_resolution(self) -> MockStream:
         return self.streams[-1]
-    
-    def get_by_resolution(self, *args, **kwargs) -> List[MockStream]:
+
+    def get_by_resolution(self, *args, **kwargs) -> MockStream:
         return self.streams[0]
-    
+
     def order_by(self, *args, **kwargs) -> List[MockStream]:
         return self.streams
-        
-        
+
+
 class MockYoutube:
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: str, **kwargs: Any) -> None:
         self.streams = MockStreamsArray()
-    
-    def register_on_progress_callback(self, *args, **kwargs) -> None:
+
+    def register_on_progress_callback(self, *args: str, **kwargs: Any) -> None:
         pass
-    
-    def register_on_complete_callback(self, *args, **kwargs) -> None:
+
+    def register_on_complete_callback(self, *args: str, **kwargs: Any) -> None:
         pass
